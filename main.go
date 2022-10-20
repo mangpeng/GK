09e7cfa2e7bd7d37a4bd190a4248b7834fe09f3d
mangpeng <rlatmdwp5@gmail.com>
2022-10-20 22:47:17 +0900

    메인 함수 정리

--- main.go
+++ main.go
@@ -1,41 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	fn := func(first int, second int) int {
-		return first + second
-	}(3, 4)
-
-	println(fn)
-}
-
-func StartInterval(interval int, count int) {
-	PlayInterval(interval, count)
-}
-
-type Color int
-
-const (
-	a Color = iota
-	b
-	c
-	d
-	e
-)
-
-func StopInterval() {
-
-}
-
-func PlayInterval(interval int, count int) {
-
-	for i := 0; i < count; i++ {
-		ch := time.After(time.Duration(interval) * time.Second)
-		<-ch
-		fmt.Println("hi")
-	}
 }
